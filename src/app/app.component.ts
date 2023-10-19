f3da06f06a2afab87a30b59becb403ecbafdb5b2
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-19 16:18:02 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [RouterModule],
+  template: `<main>
+  <header class="brand-name">
+    <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+  </header>
+  <section class="content">
+    <router-outlet></router-outlet>
+  </section>
+</main>
+`,
+styleUrls: ['./app.component.css'],
+standalone: true
 })
 export class AppComponent {
-  title = 'houses';
+  title = 'homes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HomeComponent,
+    HousingLocationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
