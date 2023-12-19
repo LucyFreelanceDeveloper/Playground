060281181448768ce1590b3e42de753e025cac34
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2023-12-19 10:36:33 +0100

    cashcard: add cashCardDeserializationTest

--- backend/spring-academy/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
+++ backend/spring-academy/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -17,6 +17,7 @@ public class CashCardJsonTest {
     @Test
     void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
+
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
@@ -24,5 +25,19 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
                 .isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+           {
+               "id":99,
+               "amount":123.45
+           }
+           """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 }
