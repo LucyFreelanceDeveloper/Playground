88d3c84cc8e8339f16dfa6d9733397127726af54
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2023-12-19 09:53:36 +0100

    Add directories and move project houses to new directory

---
+++ frontend/web-apps-example/houses/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true
+})
+export class AppComponent {
+  title = 'homes';
+}
