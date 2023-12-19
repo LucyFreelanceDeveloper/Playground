4c0c5bc7561f0668348546ff17faff6e30c2e7e8
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2023-12-19 16:10:02 +0100

    cashcard: update sort to sort by in findAll in CashCardController

--- backend/spring-academy/cashcard/src/main/java/example/cashcard/CashCardController.java
+++ backend/spring-academy/cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,7 +51,7 @@ public class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
     }
