e8227dddf4c3f5947a0d6e73b877dd412866d233
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2023-12-20 13:43:30 +0100

    cashcard: add updateCashCard mapping

--- backend/spring-academy/cashcard/src/main/java/example/cashcard/CashCardController.java
+++ backend/spring-academy/cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -5,16 +5,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Optional;
 import java.net.URI;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -57,5 +53,13 @@ public class CashCardController {
                         pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
+    }
+
+    @PutMapping("/{requestedId}")
+    private ResponseEntity<Void> updateCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
+        CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
+        CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+        cashCardRepository.save(updatedCashCard);
+        return ResponseEntity.noContent().build();
     }
 }
