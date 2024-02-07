03b739cc442a7989a1577596cbedd2408b82dd76
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2024-02-07 13:58:06 +0100

    turn-combat-example-application: update Fighter class

--- backend/turn-combat-example/src/main/java/com/example/turncombatexample/Fighter.java
+++ backend/turn-combat-example/src/main/java/com/example/turncombatexample/Fighter.java
@@ -9,35 +9,63 @@ public class Fighter {
     private Dice dice;
     private String lastMessage;
 
-    public Fighter(String name, int life, int maxLife, int attack, int defence, Dice dice, String lastMessage) {
+    public Fighter(String name, int life, int attack, int defence, Dice dice) {
         this.name = name;
         this.life = life;
-        this.maxLife = maxLife;
+        this.maxLife = life;
         this.attack = attack;
         this.defence = defence;
         this.dice = dice;
-        this.lastMessage = lastMessage;
     }
 
     public void doAttack(Fighter enemy) {
+        int hit = attack + dice.roll();
+        setMessage(String.format("%s attacking with hit %s HP", name, hit));
+        enemy.defenceYourself(hit);
     }
 
     public void defenceYourself(int hit) {
-
+        int injury = hit - (defence + dice.roll());
+        if (injury > 0) {
+            life -= injury;
+            lastMessage = String.format("%s get hurt by %s HP", name, injury);
+            if (life <= 0) {
+                life = 0;
+                lastMessage += " and died.";
+            }
+        } else {
+            lastMessage = String.format("%s counter the attack", name);
+        }
+        setMessage(lastMessage);
     }
 
     public boolean isAlive() {
-        return true;
+        return life > 0;
     }
 
-    private void setMessage() {}
+    private void setMessage(String message) {
+        this.lastMessage = message;
+    }
 
     public String returnLastMessage() {
         return lastMessage;
     }
 
     public String graphicRepresentationOfLife() {
-        return "";
+        String graphicLife = "[";
+        int sum = 20;
+        double numberOfPieces = Math.round(((double) life / maxLife) * sum);
+        if ((numberOfPieces == 0) && (isAlive())) {
+            numberOfPieces = 1;
+        }
+        for (int i = 0; i < numberOfPieces; i++) {
+            graphicLife += "#";
+        }
+        for (int i = 0; i < sum - numberOfPieces; i++) {
+            graphicLife += " ";
+        }
+        graphicLife += "]";
+        return graphicLife;
     }
 
     @Override
