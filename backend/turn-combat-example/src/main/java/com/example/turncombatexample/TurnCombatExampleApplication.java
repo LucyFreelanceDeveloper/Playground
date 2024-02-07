8fdb96c86f729ba8a5e679d05c921b751d70f6b0
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2024-02-07 13:58:30 +0100

    turn-combat-example-application: add logic to apllication

--- backend/turn-combat-example/src/main/java/com/example/turncombatexample/TurnCombatExampleApplication.java
+++ backend/turn-combat-example/src/main/java/com/example/turncombatexample/TurnCombatExampleApplication.java
@@ -8,6 +8,12 @@ public class TurnCombatExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurnCombatExampleApplication.class, args);
-	}
 
+		Dice dice = new Dice();
+		Fighter hero = new Fighter("Hero", 10, 20, 10, dice);
+		Fighter enemy = new Fighter("Shadow", 10, 15, 15, dice);
+		Arena arena = new Arena(hero, enemy, dice);
+
+		arena.battle();
+	}
 }
